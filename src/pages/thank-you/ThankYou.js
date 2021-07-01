63214d4016c1f5e31f78d2451f3addc38ebf239c
Joohyun Park <jhp527@nyu.edu>
2021-07-01 12:30:19 -0400

    fix typo

--- src/pages/thank-you/ThankYou.js
+++ src/pages/thank-you/ThankYou.js
@@ -43,7 +43,7 @@ const ThankYou = () => {
         </h1>
 
         <p className="form-legal">
-          For more information about how BNY Mellon Wealth Management uses your person
+          For more information about how BNY Mellon Wealth Management uses your personal
           information,&nbsp;
           <a
             href="https://urldefense.com/v3/__https:/www.bnymellonwealth.com/privacy.jsp__;!!IHJ3XrWN4X8!ckHdrtGPf1zGtIW7me8g94AlV1m1UX8ypeDDr3GeAa0b5158HTCwFB41v5pQNXKAUA$"
